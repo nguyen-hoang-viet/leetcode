98c859c2d9045d6443fd7089c1faaa6dca2625be
VietNH55 <nguyenhoangviet9a5dd@gmail.com>
2025-09-25 17:33:35 +0700

    first commit

---
+++ (Leetcode_414)Third_Maximum_number.py
@@ -0,0 +1,11 @@
+# Problem: Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+#---------------------------------------------------------
+# Define steps to solve the problem
+# - Understand the problem
+# - Design solutions
+# - Implement
+# - Test => manually walk them
+# - Optimize?
+#---------------------------------------------------------
+
