1b2f147e6d705168abd6bd9aeba67e76d2e7456b
VietNH55 <nguyenhoangviet9a5dd@gmail.com>
2025-09-30 20:11:20 +0700

    understand the problem and desisgn some solutions to solve this problem

--- (Leetcode_414)Third_Maximum_number.py
+++ (Leetcode_414)Third_Maximum_number.py
@@ -3,9 +3,14 @@
 #---------------------------------------------------------
 # Define steps to solve the problem
 # - Understand the problem
+#   + đọc kỹ đề bài hoặc hỏi người phỏng vấn để hiểu rõ yêu cầu
 # - Design solutions
+#   + Brainstorming: liệt kê các cách tiếp cận khác nhau cho người phỏng vấn
 # - Implement
+#   + Triển khai giải pháp đã chọn
 # - Test => manually walk them
+#   + Test với các phương pháp đã triển khai
 # - Optimize?
+#   + Tối ưu hóa giải pháp nếu cần thiết
 #---------------------------------------------------------
 
